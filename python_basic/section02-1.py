f5a9375bd0f30d18b92f50e564dbbdcfef6097b5
Paul Lee <ba93love@gmail.com>
2019-06-19 21:14:59 +0900

    Update section02-1.py

--- python_basic/section02-1.py
+++ python_basic/section02-1.py
@@ -29,3 +29,14 @@ print("%s's favorite number is %d"% ('WoolBro',7)) # %s : 문자 , %d : 정수, 
 print("Test1 : %5d, Price: %4.2f"%(700,6543.211))
 print("Test1 : {0: 5d}, Price:{1: 4.2f}".format(700,6543.211))
 print("Test1 : {a:5d}, Price:{b: 4.2f}".format(a=700,b=6543.211))
+
+
+
+print(""" Lorem Ipsum is simply dummy te
+xt of the printing and typesetting industry. Lorem Ipsum h
+as been the industry's standard dummy text ever since the 1500s, when an unknow
+n printer took a galley of type and scrambled it to make a type specimen book. It has 
+survived not only five centuries, but also the leap into electronic typesetting, remaining
+ essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets c
+ ontaining Lorem Ipsum passages, and more recently with desktop
+ publishing software like Aldus PageMaker including versions of Lorem Ipsum.""")
